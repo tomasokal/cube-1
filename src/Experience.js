ffc6b6299598c7c65a4114e065126479db85e786
Tomas Okal <tomas.okal@gmail.com>
2022-12-04 22:30:18 -0600

    Add position and scale change for boxes.

--- src/Experience.js
+++ src/Experience.js
@@ -15,37 +15,35 @@ export default function Experience()
 
     useFrame((state, delta) =>
     {
-
+        
+        // Get time variable.
         const t = state.clock.getElapsedTime() 
 
+        // Set up a clamp function.
         const clamp = (num, min, max) => Math.min(Math.max(num, min), max)
 
-        // console.log(Math.cos(2 * Math.PI / 3))
-
+        // Change position of ball along x, y, z axes.
         ball.current.position.y = Math.abs( Math.cos(2 * t) ) + 1
-
-        // ball.current.position.x = Math.cos(t) / 2 + 0.5
-        // ball.current.position.z = Math.cos(t) / 2 + 0.5
-
-        // cubeOne.current.scale.y = Math.cos(t / 2) * 2
-        // cubeTwo.current.scale.y = Math.sin(t * 2 + Math.PI / 2) / 2 + 1.5
-        // cubeThree.current.scale.y = Math.sin(t * 2 + Math.PI) / 2 + 1.5
-        // cubeFour.current.scale.y = Math.sin(t * 2 + 3 * Math.PI / 2) / 2 + 1.5
-
         ball.current.position.x = clamp( Math.sin(t), -2 / 3, 2 / 3 )
         ball.current.position.z = clamp( Math.sin(t + Math.PI / 2), -2 / 3, 2 / 3 )
-        // ball.current.position.x = Math.sin(t)
-        // console.log(ball.current.position.x)
-        // ball.current.position.x = Math.cos(t) + 0.5
-        // ball.current.position.z = Math.cos(t) / 2 + 0.5
-        // console.log(Math.cos(t) + 0.5)
-        // console.log(Math.cos( Math.PI / 3 ))
-        // console.log(Math.cos( 2 * Math.PI / 3 ))
-        // ball.current.position.z = clamp(Math.sin(t) + 0.5, 0, 1)
 
-        // ball.current.position.y = ( Math.cos(t * 2) ) + 2
-        // ball.current.position.z = Math.sin(t) / 2 + 0.5
-        // ball.current.position.y = Math.sin(t * 4) / 2 + 2
+        // Ball bounces every pi / 2
+            // First bounce is at pi / 4
+            // Second bounce is at 3 * pi / 4
+            // Third bounce is at 5 * pi / 4
+            // Fourth bounce is at 7 * pi / 4
+
+        cubeOne.current.scale.y = clamp( Math.cos(t + 3 * Math.PI / 4), 0.5, 1 )
+        cubeOne.current.position.y = cubeOne.current.scale.y / 2 - 0.5
+
+        cubeTwo.current.scale.y = clamp( Math.cos(t + 5 * Math.PI / 4), 0.5, 1 )
+        cubeTwo.current.position.y = cubeTwo.current.scale.y / 2 - 0.5
+
+        cubeThree.current.scale.y = clamp( Math.cos(t + 7 * Math.PI / 4), 0.5, 1 )
+        cubeThree.current.position.y = cubeThree.current.scale.y / 2 - 0.5
+
+        cubeFour.current.scale.y = clamp( Math.cos(t + 9 * Math.PI / 4), 0.5, 1 )
+        cubeFour.current.position.y = cubeFour.current.scale.y / 2 - 0.5
 
     })
 
@@ -57,14 +55,7 @@ export default function Experience()
 
         <Stage>
 
-            {/* <mesh position={ [0, 0, -1] } >
-                <planeGeometry args={ [ 5, 5 ] } />
-                <meshBasicMaterial 
-                    color={'#F8D628'}    
-                />
-            </mesh> */}
-
-            <mesh position={ [-0.5, 0, -0.5] } ref={ cubeOne } >
+            <mesh position={ [ -0.5, 0, -0.5 ] } ref={ cubeOne } >
                 <boxGeometry args={ [ 1, 1, 1 ] } />
                 <meshPhysicalMaterial 
                     metalness={ 0 }
@@ -104,7 +95,7 @@ export default function Experience()
                 />
             </mesh>
 
-            <mesh position={ [0, 2, 0] }  ref={ ball }>
+            <mesh position={ [0, 1, 0] }  ref={ ball }>
                 <sphereGeometry args = { [ 0.5, 64 ] } />
                 <meshPhysicalMaterial 
                     metalness={ 0 }
